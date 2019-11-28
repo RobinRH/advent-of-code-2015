3e3bc57b990e49932e3d1ceec946ef2499b9fd89
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-28 01:10:38 -0800

    Update day06-1.py

--- day06/day06-1.py
+++ day06/day06-1.py
@@ -4,7 +4,6 @@
 # x through y -> x is top left of rectangle, y is bottom right of rectangle
 
 import sys
-import re
 
 # turn on 296,50 through 729,664
 # toggle 756,965 through 812,992
@@ -14,15 +13,9 @@ filename = sys.argv[1]
 with open(filename, 'r') as inputFile:
     lines = inputFile.readlines()
 
-instructions = []
-['turn', ' ', 'on', ' ', '489', ',', '959', ' ', 'through', ' ', '759', ',', '964\n']
-for line in lines:
-    tokens = re.split(r"( |,)", line)
-    if len(tokens) == 13: # turn on/ turn off
-        ins = [tokens[2], int(tokens[4]), int(tokens[6]), int(tokens[10]), int(tokens[12])]
-    else:    # toggle
-        ins = [tokens[0], int(tokens[2]), int(tokens[4]), int(tokens[8]), int(tokens[10])]
-    instructions.append(ins)
+lines = [line.strip().replace('toggle', 'turn toggle').replace(' ', ',') for line in lines]
+instructions = [line.split(',') for line in lines]
+instructions = [[i[1], int(i[2]), int(i[3]), int(i[5]), int(i[6])] for i in instructions]
 
 # set up the array
 lights = [[False for x in range(1000)] for y in range(1000)]
@@ -40,10 +33,6 @@ for ins in instructions:
             else:
                 lights[row][col] = not lights[row][col]
 
-onlights = 0
-for row in range(0, 1000):
-    for col in range(0, 1000):
-        if lights[row][col] == True:
-            onlights += 1
+onlights = sum(sum(row) for row in lights)
 
-print onlights
+print (onlights)
