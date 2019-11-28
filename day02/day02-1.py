e83b3ab1f69127223f24b39fecc2bed9d366cada
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-27 23:50:46 -0800

    Update day02-1.py

--- day02/day02-1.py
+++ day02/day02-1.py
@@ -10,12 +10,9 @@ filename = sys.argv[1]
 with open(filename, 'r') as inputFile:
     packageSizes = inputFile.readlines()
 
-total = 0
-for package in packageSizes:
-    [l, w, h] = package.strip().split("x")
-    [l, w, h] = map((lambda x : int(x)), [l, w, h])
-    total += 2 * (l*w + w*h + l*h)
-    smallest = min([l*w, w*h, l*h])
-    total += smallest
-
-print total
+packages = [package.strip().split('x') for package in packageSizes]
+packages = [(int(x), int(y), int(z)) for (x, y, z) in packages]
+areas = [2 * (l*w + w*h + l*h) for (l, w, h) in packages]
+smallest = [min([l*w, w*h, l*h]) for (l, w, h) in packages]
+total = sum(areas) + sum(smallest)
+print(total)
