981377441c156645496a8e04317fb2dba874dd77
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-28 00:11:47 -0800

    Update day04-1.py

--- day04/day04-1.py
+++ day04/day04-1.py
@@ -1,16 +1,18 @@
-# 282749
-# find lowest start+integer, such that MD4 hex digest starts with "00000"
+# 9962624 (takes 3 minutes or so)
+# find lowest start+integer, such that MD4 hex digest starts with "000000"
 
 import hashlib
 
 start = 'yzbqklnj'
 count = 0
 
-for count in range(0, 2000000):
+found = False
+while not found:
     string = start + str(count)
     m = hashlib.md5()
     m.update(string.encode('utf-8'))
     digest = m.hexdigest()
-    if str(digest).startswith("00000"):
+    if str(digest).startswith("000000"):
         print(count, digest)
         break
+    count += 1
