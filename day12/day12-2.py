c54514a22067fcfa6413ddef1be6f32350c5a77e
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-28 22:32:12 -0800

    Update day12-2.py

--- day12/day12-2.py
+++ day12/day12-2.py
@@ -4,9 +4,7 @@
 # https://adventofcode.com/2015/day/12
 
 import sys
-import re
 import json
-import pprint
 
 filename = sys.argv[1]
 with open(filename, 'r') as inputFile:
@@ -14,60 +12,38 @@ with open(filename, 'r') as inputFile:
 
 jsonObj = json.loads(jsonString)
 
-reds = 0
-
 def countNumbers(dictionary):
-    global reds
     total = 0
-    # items in the dictionary are dict (object), list (array), int (integer), unicode (string)
+    # items in the dictionary are dict (object), list (array), int (integer), str (string)
     for child in dictionary.values():
         if type(child) is dict:
-            hasNoRed = True
             # find out if it has an property with value "red"
-            for property in child.values():
-                if (type(property) is unicode) and (property == "red"):
-                    reds += 1
-                    hasNoRed = False
-
-            if hasNoRed:
+            if not 'red' in child.values():
                 total += countNumbers(child)
-
         elif type(child) is list:
             total += countNumbersinList(child)
-
         elif type(child) is int:
             total += int(child)
-
-        elif type(child) is unicode:
+        elif type(child) is str:
             pass
 
     return total
 
 def countNumbersinList(aList):
-    global reds
     total = 0
     for item in aList:
-        #print type(item)
         if type(item) is dict:
-            #total += countNumbers(item)
-            hasNoRed = True
             # find out if it has an property with value "red"
-            for property in item.values():
-                if (type(property) is unicode) and (property == "red"):
-                    reds += 1
-                    hasNoRed = False
-
-            if hasNoRed:
+            if not 'red' in item.values():
                 total += countNumbers(item)
-
         elif type(item) is list:
             total += countNumbersinList(item)
         elif type(item) is int:
             total += int(item)
-        elif type(item) is unicode:
+        elif type(item) is str:
             pass
 
     return total
 
-print countNumbersinList(jsonObj)
+print(countNumbersinList(jsonObj))
 
