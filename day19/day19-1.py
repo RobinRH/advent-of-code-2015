232d0b34991a33f2152c601c886faab705877817
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-29 23:33:25 -0800

    Update day19-1.py

--- day19/day19-1.py
+++ day19/day19-1.py
@@ -5,45 +5,22 @@ import sys
 import re
 import pprint
 
+# Al => ThRnFAr
 rulesFilename = sys.argv[1]
 with open(rulesFilename, 'r') as inputFile:
-    lines = inputFile.readlines()
+    lines = [line.strip().replace('=> ', '').split(' ') for line in inputFile.readlines()]
 
-#stringFilename = sys.argv[2]
-#with open(stringFilename, 'r') as stringFile:
-#    input = stringFile.read()
-
-# Al => ThF
-# Al => ThRnFAr
-
-replacements = []
-ruleExpr = re.compile('(?P<in>.+) => (?P<out>.+)')
-for line in lines:
-    if line == "":
-        break
-    match = ruleExpr.match(line)
-    if match:
-        replacements.append((match.group('in'), match.group('out')))
-
-input = lines[-1]
+replacements = [(a, b) for a, b in lines[0:len(lines)-2]]
+input = lines[-1][0]
 
 words = set()
 # only replace one at a time
 for repl in replacements:
+    starts = [match.start() for match in re.finditer(repl[0], input)]
+    length = len(repl[0])
+    additions = [input[:start] + repl[1] + input[start + length:] for start in starts]
+    words.update(additions)
 
-    starts = []
-    for match in re.finditer(repl[0], input):
-        starts.append(match.start())
-
-    for start in starts:
-        # get string before start
-        part1 = input[:start]
-        # get second part of string
-        part2 = input[start + len(repl[0]):]
-        word = "".join((part1, repl[1], part2))
-        #print word 
-        words.add(word)
-
-print len(words)
+print(len(words))
 
 
