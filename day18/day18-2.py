1e0f7ceb4eaa100556b1fc8f656369c147c31bfb
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-29 23:05:24 -0800

    Update day18-2.py

--- day18/day18-2.py
+++ day18/day18-2.py
@@ -5,30 +5,18 @@
 import sys
 import pprint
 import numpy as np 
-import scipy
 
 filename = sys.argv[1]
 
 with open(filename, 'r') as inputFile:
-    data = inputFile.read()
+    data = [0 if x == '.' else 1 for x in inputFile.read().replace('\n', '')]
 
-data = data.replace("\n","")
-
-data = list(map(lambda x: 0 if x == "." else 1, data))
-npData = np.array(data)
-npData = np.resize(npData, (100,100))
+npData = np.resize(data, (100,100))
 padded = np.pad(npData, 1, 'constant')
 
 
-kernel = np.array([[1, 1, 1],
-                   [1, 0, 1],
-                   [1, 1, 1]])
-
 def getNewValue(grid, row, col):
-
-    sum = grid[row-1, col-1] + grid[row-1, col] + grid[row-1, col + 1] + \
-        grid[row, col-1] + grid[row, col + 1] + \
-        grid[row+1, col-1] + grid[row+1, col] + grid[row+1, col + 1]
+    sum = np.sum(grid[row-1:row+2, col-1:col+2]) - grid[row, col]
     # A light which is on stays on when 2 or 3 neighbors are on, and turns off otherwise.
     # A light which is off turns on if exactly 3 neighbors are on, and stays off otherwise.
     if grid[row, col] == 1:
@@ -51,8 +39,6 @@ for step in range(0, 100):
     newGrid = np.zeros((102, 102), dtype = int)
     for row in range(1, 101):
         for col in range(1, 101):
-            newVal = getNewValue(padded, row, col)
-            newGrid[row, col] = newVal
             newGrid[row, col] = getNewValue(padded, row, col)
     padded = newGrid
     padded[1,1] = 1
@@ -61,4 +47,4 @@ for step in range(0, 100):
     padded[100, 100] = 1
 
 
-print np.sum(padded)
+print(np.sum(padded))
