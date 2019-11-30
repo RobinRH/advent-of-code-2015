a309dc79013961edfd516ca09a53e3a6da07698a
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-11-29 20:40:25 -0800

    Update day17-1.py

--- day17/day17-1.py
+++ day17/day17-1.py
@@ -1,25 +1,29 @@
-# 4372
+# part 1: 4372
+# part 2: 4
 # find all combinations of containers that add up to 150
 # https://adventofcode.com/2015/day/17
 
 import sys
-import pprint
 import itertools
 
 filename = sys.argv[1]
-
 with open(filename, 'r') as inputFile:
-    lines = inputFile.readlines()
-
-sizes = list(map(lambda x : int(x), lines))
+    sizes = [int(line) for line in inputFile.readlines()]
 
 count = 0
 for length in range(0, len(sizes)):
     containerSets = itertools.combinations(sizes, length)
-    for oneSet in containerSets:
-        capacity = sum(oneSet)
-        if capacity == 150:
-            #pprint.pprint(oneSet)
-            count += 1
+    count += len([1 for oneset in containerSets if sum(oneset) == 150])
 
-print count
+print('part 1: ', count)
+
+
+# find all the set where capacity = 150
+allSets = []
+for length in range(0, len(sizes)):
+    containerSets = itertools.combinations(sizes, length)
+    allSets.extend([oneset for oneset in containerSets if sum(oneset) == 150])
+
+minLength = min([len(x) for x in allSets])
+minNumber = len([x for x in allSets if len(x) == 4])
+print('part 2: ', minNumber)
